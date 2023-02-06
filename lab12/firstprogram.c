965ab94a50d58190a6c352d4437a30e8aa0b5b1a
Ha Hoang <hahoang@venom.local>
2023-02-06 17:32:03 +0700

    first commit

---
+++ lab12/firstprogram.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// func to add 2 numbers
+int add2Number(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int result = add2Number(3, 5);
+    printf("Tong 2 so la: %d\n", result);
+    return 0;
+}
